a851ecfd0d978f426fb0f99567fa9388b8c6ad01
mobus <sv0220@163.com>
2023-07-12 17:11:32 +0800

    finish basic framework

--- src/pkg/handler/subscribe.rs
+++ src/pkg/handler/subscribe.rs
@@ -1,3 +1,20 @@
-pub async fn handler() {
-    println!("Hello, info handler!");
+#![allow(dead_code, unused_imports)]
+use axum::{
+    http::{Request, Response, Result, StatusCode},
+    response::IntoResponse,
+    Json,
+};
+use log::info;
+use serde::{Deserialize, Serialize};
+
+pub async fn handler(Json(req): Json<SubReq>) -> impl IntoResponse {
+    info!("subscribe handler");
+    info!("req: {:?}", req);
+    (StatusCode::OK, r#"{"success": true}"#)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SubReq {
+    pub topic: String,
+    pub webhook: String,
 }
